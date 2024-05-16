e6ab0d3878f937aec981686395cee65c74572c7c
FYIT25KUNAL <98008881+FYIT25KUNAL@users.noreply.github.com>
2024-05-16 20:09:18 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Hello!! Welcome to Band Name Generator!!!")
+
+city = input("What is the name of your city? ")
+pet = input("What is the name of your city? ")
+
+print("The Name of your Band is: " + city + " " + pet)
