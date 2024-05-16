1be2318b0352f8f35dd58d6bc6bba1871ea62a19
FYIT25KUNAL <98008881+FYIT25KUNAL@users.noreply.github.com>
2024-05-16 20:14:55 +0530

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,6 +1,8 @@
+#First is a simple greeting message.
 print("Hello!! Welcome to Band Name Generator!!!")
-
+# We define two variable and ask the user using the 'input' function.
 city = input("What is the name of your city? ")
 pet = input("What is the name of your city? ")
-
+# Once the user has provided the inputs, we concatinate the variables together and print it alltogether.
 print("The Name of your Band is: " + city + " " + pet)
+#Thank You!!!
